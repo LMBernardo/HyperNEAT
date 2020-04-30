6ee2a05e5239ea99dfc6f3536c1d9ef056cf8cd8
Layne Bernardo <lmbernar@uark.edu>
2020-04-30 02:46:06 -0500

    Build debug as well
    Signed-off-by: Layne Bernardo <lmbernar@uark.edu>

--- buildAll.sh
+++ buildAll.sh
@@ -77,15 +77,15 @@ mkdir -p build
 pushd build
 mkdir -p debug
 mkdir -p release
-#pushd debug
-#cmake -DCMAKE_BUILD_TYPE=Debug ../../
+pushd debug
+cmake -DCMAKE_BUILD_TYPE=Debug ../../
 # The first run forces release, so run again to override
-#cmake -DCMAKE_BUILD_TYPE=Debug ../../
-#make -j8
-#popd
+cmake -DCMAKE_BUILD_TYPE=Debug ../../
+make -j8
+popd
 pushd release
 cmake -DCMAKE_BUILD_TYPE=Release ../../
-make VERBOSE=1 -j8
+make -j8
 popd
 popd
 popd
