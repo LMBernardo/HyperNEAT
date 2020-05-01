8ebee6fda17dcf20dd0c6c081dc8681557c1faad
Layne Bernardo <lmbernar@uark.edu>
2020-04-30 23:24:54 -0500

    Removed zlib and tinyxml - they are available in the Arch repos
    Signed-off-by: Layne Bernardo <lmbernar@uark.edu>

--- buildAll.sh
+++ buildAll.sh
@@ -1,41 +1,5 @@
 set -e
 
-pushd zlib
-mkdir -p build
-pushd build
-mkdir -p debug
-mkdir -p release
-pushd debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../../
-# The first run forces release, so run again to override
-cmake -DCMAKE_BUILD_TYPE=Debug ../../
-make -j8
-popd
-pushd release
-cmake -DCMAKE_BUILD_TYPE=Release ../../
-make -j8
-popd
-popd
-popd
-
-pushd tinyxmldll
-mkdir -p build
-pushd build
-mkdir -p debug
-mkdir -p release
-pushd debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../../
-# The first run forces release, so run again to override
-cmake -DCMAKE_BUILD_TYPE=Debug ../../
-make -j8
-popd
-pushd release
-cmake -DCMAKE_BUILD_TYPE=Release ../../
-make -j8
-popd
-popd
-popd
-
 pushd Board
 mkdir -p build
 pushd build
